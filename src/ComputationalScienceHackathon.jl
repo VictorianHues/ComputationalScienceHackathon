7afca375152d6b58f44917dee34223d6b2f09b23
VictorianHues <VictorianHues@gmail.com>
2025-06-16 16:27:37 +0200

    Additional cleaning and clerification of experiments

--- src/ComputationalScienceHackathon.jl
+++ src/ComputationalScienceHackathon.jl
@@ -13,6 +13,6 @@ include("plotting.jl")
 # Optionally, export functions or types you want users to access directly
 # export my_function, MyType
 
-export swe_dae_residual!, timeloop, plot_solution_animation
+export swe_dae_residual!, swe_dae_residual_dirichlet_neumann!, timeloop, plot_solution_animation
 
 end
--- src/main.jl
+++ src/main.jl
@@ -15,16 +15,17 @@ function make_parameters(bed_level="flat")
 
     cf = 0.00232  # friction coefficient
 
+    # Bed level definition
     if bed_level == "flat"
         zb = fill(-D, N)  # flat bottom
     elseif bed_level == "wavy"
-        zb = -D .+ 1.0 .* sin.(2*π*5 .* x ./ xmax .* ((N - 1) / N))
+        zb = -D .+ 5.0 .* sin.(2*π*3 .* x ./ xmax .* ((N - 1) / N))
     elseif bed_level == "peak"
-        zb = -D .+ 1.0 .* exp.(-0.5 .* ((x .- xmax/2) ./ (0.2 * xmax)).^2)  # normal distribution peak
+        zb = -D .+ 5.0 .* exp.(-0.5 .* ((x .- xmax/2) ./ (0.2 * xmax)).^2)  # normal distribution peak
     elseif bed_level == "incline"
-        zb = -D .+ 1.0 .* (x ./ xmax)  # linear incline
+        zb = -D .+ 5.0 .* (x ./ xmax)  # linear incline
     elseif bed_level == "decline"
-        zb = -D .+ 1.0 .* (1 .- x ./ xmax)  # linear decline
+        zb = -D .+ 5.0 .* (1 .- x ./ xmax)  # linear decline
     else
         error("Unknown bed level type: $bed_level")
     end
@@ -36,6 +37,7 @@ function initial_conditions(params, u, h_initial="peak")
     @unpack N, x, zb = params
     xmax = maximum(x)
 
+    # Initialize water height h
     if h_initial == "peak"
         h = 1.0 .* exp.(-100 .* ((x ./ xmax .- 0.5) .* xmax).^2) .- zb 
     elseif h_initial == "flat"
@@ -45,108 +47,41 @@ function initial_conditions(params, u, h_initial="peak")
     else
         error("Unknown initial condition type: $h_initial")
     end
-    
+
     q = h .* u
 
     return h, q
 end
 
 function main()
-    u_initial = 0.0
-    file_name = "swe_single_peak_wavy_bottom.gif"
-    params = make_parameters("wavy")
-    h0, q0 = initial_conditions(params, u_initial, "peak")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
+    # Define experiment configurations
+    bed_levels = ["wavy", "flat", "peak", "incline", "decline"]
+    water_inits = ["peak", "flat"]
+    boundary_conditions = ["dirichlet_neumann", "periodic"]
+    u_initials = [0.0, 0.5]
+    discharge_labels = ["", "_discharge"]
 
-    file_name = "swe_single_peak_flat_bottom.gif"
-    params = make_parameters("flat")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
+    for (i, u_initial) in enumerate(u_initials)
+        for water_init in water_inits
+            for bed_level in bed_levels
+                for bc in boundary_conditions
+                    # Only run "peak" water_init for u_initial == 0.0 and u_initial == 0.5
+                    # Only run "flat" water_init for u_initial == 0.5
+                    if (water_init == "flat" && u_initial == 0.0)
+                        continue
+                    end
 
-    file_name = "swe_single_peak_peak_bottom.gif"
-    params = make_parameters("peak")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_peak_incline_bottom.gif"
-    params = make_parameters("incline")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_peak_decline_bottom.gif"
-    params = make_parameters("decline")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-
-    u_initial = 0.5
-    file_name = "swe_single_flat_wavy_bottom_discharge.gif"
-    params = make_parameters("wavy")
-    h0, q0 = initial_conditions(params, u_initial, "flat")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_flat_flat_bottom_discharge.gif"
-    params = make_parameters("flat")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_flat_peak_bottom_discharge.gif"
-    params = make_parameters("peak")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_flat_incline_bottom_discharge.gif"
-    params = make_parameters("incline")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params)
-    plot_solution_animation(solution, params, file_name)
-    file_name = "swe_single_flat_decline_bottom_discharge.gif"
-    params = make_parameters("decline")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params)
-    plot_solution_animation(solution, params, file_name)
-
-
-    u_initial = 0.5
-    file_name = "swe_single_peak_wavy_bottom_discharge.gif"
-    params = make_parameters("wavy")
-    h0, q0 = initial_conditions(params, u_initial, "peak")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_peak_flat_bottom_discharge.gif"
-    params = make_parameters("flat")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_peak_peak_bottom_discharge.gif"
-    params = make_parameters("peak")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params) 
-    plot_solution_animation(solution, params, file_name)
-
-    file_name = "swe_single_peak_incline_bottom_discharge.gif"
-    params = make_parameters("incline")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params)
-    plot_solution_animation(solution, params, file_name)
-    file_name = "swe_single_peak_decline_bottom_discharge.gif"
-    params = make_parameters("decline")
-    params = merge(params, (; h0, q0))
-    solution = timeloop(params)
-    plot_solution_animation(solution, params, file_name)
+                    discharge_label = discharge_labels[i]
+                    file_name = "swe_$(water_init)_water_$(bed_level)_bottom_$(bc)$(discharge_label).gif"
+                    params = make_parameters(bed_level)
+                    h0, q0 = initial_conditions(params, u_initial, water_init)
+                    params = merge(params, (; h0, q0))
+                    solution = timeloop(params, bc)
+                    plot_solution_animation(solution, params, file_name)
+                end
+            end
+        end
+    end
 
 
 end
